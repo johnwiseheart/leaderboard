154018aa253cfd47dfedf2d969321266e22b2ffd
John Wiseheart <johnwiseheart@gmail.com>
2014-08-05 12:40:38 +1000

    Added stuff

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,4 @@
-from wtforms.validators import Required, DataRequired
+from wtforms.validators import Required, DataRequired, Regexp
 from flask_security.forms import RegisterForm
 from wtforms import TextField, PasswordField
 from flask.ext.security import Security
@@ -7,7 +7,7 @@ from flask.ext.wtf import Form
 from models import Team
 
 class ExtendedRegisterForm(RegisterForm):
-    name = TextField('Team Name', [validators.Regexp(r'^[\w._-]+$'), DataRequired()])
+    name = TextField('Team Name', [Regexp(r'^[\w._-]+$'), DataRequired()])
     players = TextField('Players', [DataRequired()])
 
 class PasswordForm(Form):
