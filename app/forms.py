9bed115377ff38c67b2726f77223d3464017774d
John Wiseheart <johnwiseheart@gmail.com>
2014-08-05 22:47:40 +1000

    Added stuff

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,4 @@
-from wtforms.validators import Required, DataRequired, Regexp
+from wtforms.validators import Required, DataRequired, Regexp, Length
 from flask_security.forms import RegisterForm
 from wtforms import TextField, PasswordField
 from flask.ext.security import Security
@@ -7,7 +7,7 @@ from flask.ext.wtf import Form
 from models import Team
 
 class ExtendedRegisterForm(RegisterForm):
-    name = TextField('Team Name', [Regexp(r'^[\w._-]+$'), DataRequired()])
+    name = TextField('Team Name', [Regexp(r'^[\w._-]+$',message='You may only use alphanumeric characters, hyphens and underscores.'), Length(min=5, max=25,message="Password must be between 5 and 25 characters."), DataRequired()])
     players = TextField('Players', [DataRequired()])
 
 class PasswordForm(Form):
